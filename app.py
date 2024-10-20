25527676c73906f67713eb13a6020729e83a1e2f
Arka <arkam27112004@gmail.com>
2024-10-21 00:14:22 +0530

    dictionary error handling

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from pprint import pprint
 import time
 
 app = Flask(__name__)
-PROB_COUNT_START_TIME = 1723260000 # 1729449000
+PROB_COUNT_START_TIME = 1729449000
 
-handles = ['qchaos', 'b23230']  # add other handles here
+handles = ['HoneyGupta', 'Divyansh21j', '141GHOST', 'Riddhi17', 'tvishajaiswal2696', 'Darkrai123', 'Nirupam', 'shubhamanand299', 'utkarshsahu123', 'Priansh_master', 'saurav10codes', 'EpicCoder03', 'AkshitaDangi2015', 'Ojasvijain', 'kdshacker359', 'DivyanshJindal', 'KingGarvit', 'suhavyyy', 'utkarshBansal', 'QuantumJunkie', 'Chirag352', 'gargsatvik06', 'SyntaxSorcerery', 'ArhamJain2506', 'timeless_tech_crafter', 'sakshamKundu', 'vipreshgupta', 'HellSlayer42', 'XeroRazer', 'Mr.N00B', 'JapneetKohli187', 'CP_GOD_', 'anishkumar96465213', 'ekansh007', 'mahak_ag', 'akshat_idk_29', 'anshika476', 'Akshat_ansh', 'Harmonixar', 'abhayxd', 'stellar_87', 'programmer_byte', 'ps2006', 'Dhanad', 'te_am0', 'hoda_goes_boom', 'lightman0', 'ananyaGawade', 'AmanGuptacd', 'kumaraman6012', 'Harsh_Yadav1729', 'Kartavya_Suryawanshi']
 method_name = 'user.info'
 params = {
     'handles': ';'.join(handles)
@@ -43,11 +43,11 @@ def users_page():
 
 @app.route('/ratings')
 def ratings_page():
-    return render_template('contest_leaderboard.html', users=sorted(users, key=lambda user: user['rating'], reverse=True))
+    return render_template('contest_leaderboard.html', users=sorted(users, key=lambda user: user.get('rating', 0), reverse=True))
 
 @app.route('/leaderboard')
 def leaderboard_page():
-    return render_template('prob_leaderboard.html', users=sorted(users, key=lambda user: user['score'], reverse=True))
+    return render_template('prob_leaderboard.html', users=sorted(users, key=lambda user: user.get('rating', 0), reverse=True))
 
 if __name__ == '__main__':
     app.run(debug=True)
